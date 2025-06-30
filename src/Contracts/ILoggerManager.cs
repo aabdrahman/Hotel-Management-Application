23f9a02e9c212c2d17978ddcb27d3cfa4efb52c4
Abdrahman Akande <Abdrahman.Akande@parallexbank.com>
2025-06-30 18:41:59 +0100

    Configure Logging Contract

---
+++ src/Contracts/ILoggerManager.cs
@@ -0,0 +1,11 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Contracts;
+
+public interface ILoggerManager
+{
+    void LogInfo(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFile = "");
+    void LogWarning(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFile = "");
+    void LogError(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFile = "");
+    void LogDebug(string message, [CallerMemberName] string callerName = "", [CallerFilePath] string callerFile = "");
+}
